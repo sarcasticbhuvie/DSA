76a54cb7cbc9228aada70a6db8841278b44dbcca
sarcasticbhuvie <92844139+sarcasticbhuvie@users.noreply.github.com>
2023-05-08 22:06:38 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+struct node{
+    int data;
+    struct node *next;
+    };
+    struct node *start=NULL;
+    void main()
+    {
+        struct node *new_node,*ptr;
+        int value;
+        printf("enter value:");
+        scanf("%d",&value);
+        while(value!=-1)
+        {
+        new_node=(struct node*)malloc(sizeof(struct node));
+        new_node->data=value;
+        if(start==NULL)
+        {
+            new_node->next=NULL;
+            start=new_node;
+        }
+        else
+        {
+            ptr=start;
+            while(ptr->next!=NULL)
+            {
+                ptr=ptr->next;
+            }
+            ptr->next=new_node;
+            new_node->next=NULL;
+        }
+        printf("enter value:");
+        scanf("%d",&value);
+        }
+        ptr=start;
+        while(ptr!=NULL)
+        {
+            printf("%d\t",ptr->data);
+            ptr=ptr->next;
+        }
+    }
+
